0413901bd5b0927c2b7cb91c8d67578eb9926546
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-30 05:28:24 -0300

    Concluíndo módulo de usuário

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/ins-clone')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/ins-clone'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
