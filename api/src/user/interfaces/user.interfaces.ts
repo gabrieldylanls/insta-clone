348f1d18cf98083e309565ba0d899bb98b99ab21
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-30 04:25:22 -0300

    Adicionando interface do usuário

---
+++ api/src/user/interfaces/user.interfaces.ts
@@ -0,0 +1,11 @@
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+  readonly id?: string;
+  readonly name?: string;
+  readonly fullName?: string;
+  readonly email?: string;
+  readonly password?: string;
+  readonly photo?: string;
+  readonly createdAt?: Date;
+}
