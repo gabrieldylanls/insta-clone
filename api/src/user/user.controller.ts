3ea2bdc6b054ffe56d32db40649d12295b8662c7
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-30 05:26:52 -0300

    Concluíndo controller do usuário

---
+++ api/src/user/user.controller.ts
@@ -0,0 +1,63 @@
+import { Controller, Get, Param, HttpCode, HttpStatus, Post, Body, Put, Delete } from '@nestjs/common';
+
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interfaces';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UserController {
+
+  constructor(
+    private readonly userService: UserService,
+  ) { }
+
+  @Get()
+  public async findAll(): Promise<User[]> {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  public async findById(@Param() params: { id: string }): Promise<User> {
+    return await this.userService.findById(params.id);
+  }
+
+  @Get('findByEmail/:email')
+  public async findByEmail(@Param() params: { email: string }): Promise<User> {
+    return await this.userService.findByEmail(params.email);
+  }
+
+  @Get('checkEmail/:email')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async checkEmail(@Param() params: { email: string }): Promise<boolean> {
+    return await this.userService.checkEmail(params.email);
+  }
+
+  @Get('checkUsername/:name')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async checkUsername(@Param() params: { name: string }): Promise<boolean> {
+    return await this.userService.checkUsername(params.name);
+  }
+
+  @Post()
+  public async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  public async update(@Param() params: { id: string }, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return await this.userService.updateUser(params.id, updateUserDto);
+  }
+
+  @Put('updatePassword/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async updatePassword(@Param() params: { id: string }, @Body() body: { password: string }): Promise<boolean> {
+    return await this.userService.updatePassword(params.id, body.password);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async delete(@Param() params: { id: string }): Promise<boolean> {
+    return await this.userService.deleteUser(params.id);
+  }
+}
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserSchema } from './schemas/user.schema';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
+  controllers: [UserController],
   providers: [UserService],
 })
 export class UserModule {}
