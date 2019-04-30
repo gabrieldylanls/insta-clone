b130349517939adc04347c236304edab1b7bd3ea
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-30 12:33:04 -0300

    Concluíndo módulo de autenticação

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/ins-clone'),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
