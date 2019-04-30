c7c1849487b98ca408a138f27b49b4211d2291fb
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-30 13:11:21 -0300

    Adicionando decorator injectable a classe JwtStrategy

--- api/src/auth/jwt.strategy.ts
+++ api/src/auth/jwt.strategy.ts
@@ -1,10 +1,11 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { UnauthorizedException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { AuthService } from './auth.service';
-import { User } from 'src/user/interfaces/user.interfaces';
+import { User } from '../user/interfaces/user.interfaces';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
@@ -14,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   public async validate(userData: User): Promise<User> {
-    const user = await this.authService.validateUser(userData);
+    const user = await this.authService.validate(userData);
     if (!user) {
       throw new UnauthorizedException();
     }
