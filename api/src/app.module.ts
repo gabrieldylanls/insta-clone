117384eff68a25a28e13d809dda1721850975228
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-05-01 14:39:56 -0300

    Adicionando módulo de post ao módulo principal

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/ins-clone'),
     UserModule,
     AuthModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
