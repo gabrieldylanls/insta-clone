0594c65f20a9eb2ce402d02d657f0b82746b0ebb
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-05-01 14:43:50 -0300

    Controller de post concluído

---
+++ api/src/post/post.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Post, Body, Get, Param, Put, HttpCode, HttpStatus, Delete } from '@nestjs/common';
+
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostService } from './post.service';
+import { Post as PostInterface } from './interfaces/post.interfaces';
+import { UpdatePostDto } from './dto/update-post.dto';
+
+@Controller('posts')
+export class PostController {
+  constructor(
+    private readonly postService: PostService,
+  ) {}
+
+  @Get()
+  public async findAll(): Promise<PostInterface[]> {
+    return await this.postService.findAll();
+  }
+
+  @Get(':id')
+  public async findById(@Param() params: { id: string }): Promise<PostInterface> {
+    return await this.postService.findById(params.id);
+  }
+
+  @Get('user/:userId')
+  public async findByUser(@Param() params: { userId: string }): Promise<PostInterface[]> {
+    return await this.postService.findByUser(params.userId);
+  }
+
+  @Post()
+  public async create(@Body() createPostDto: CreatePostDto): Promise<PostInterface> {
+    return await this.postService.createPost(createPostDto);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async update(@Param() params: { id: string }, @Body() updatePostDto: UpdatePostDto): Promise<boolean> {
+    return await this.postService.updatePost(params.id, updatePostDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async delete(@Param() params: { id: string }): Promise<boolean> {
+    return await this.postService.deletePost(params.id);
+  }
+}
