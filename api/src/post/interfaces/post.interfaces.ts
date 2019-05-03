546ae035057d1077e63820f8e7087122f444f080
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-05-03 15:52:16 -0300

    Alterando tipo do campo author

--- api/src/post/interfaces/post.interfaces.ts
+++ api/src/post/interfaces/post.interfaces.ts
@@ -1,12 +1,10 @@
 import { Document } from 'mongoose';
 
-import { User } from '../../user/interfaces/user.interfaces';
-
 export interface Post extends Document {
   readonly id?: string;
   readonly title?: string;
   readonly content?: string;
   readonly photo?: string;
-  readonly author?: User;
+  readonly author?: string;
   readonly createdAt?: Date;
 }
